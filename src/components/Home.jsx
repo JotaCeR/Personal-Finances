0eda163651af9104385275193949c92103abd5b9
Juan Roberts <juancroberts31@gmail.com>
2021-09-28 22:03:05 -0300

    Dumb Home component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,14 @@ export default function Home() {
     return (
         <div>
             <h1>This is the Home</h1>
+            <div>
+                <h2>Current Balance</h2>
+                <p>0</p>
+            </div>
+            <div>
+                <h2>Last Entries</h2>
+                <p>Empty. .  .</p>
+            </div>
         </div>
     )
 }
