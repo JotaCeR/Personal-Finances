04c37de1c91f6943df01e73bddb1949c3e0d8f9e
Juan Roberts <juancroberts31@gmail.com>
2021-09-28 22:48:36 -0300

    Dumb new entries form created.

--- src/components/OpsABM.jsx
+++ src/components/OpsABM.jsx
@@ -4,6 +4,17 @@ export default function ABM() {
     return (
         <div>
             <h1>ABM Operations</h1>
+            <div>
+                <h3>New Entry</h3>
+                <form>
+                    <input name="reason" type="text" />
+                    <input name="amount" type="number" min="0.1" />
+                    <input name="date" type="date" />
+                    <input name="extraction" value="add" type="checkbox" />
+                    <input name="sustraction" value="rem" type="checkbox" />
+                    <button>Add</button>
+                </form>
+            </div>
         </div>
     )
 }
