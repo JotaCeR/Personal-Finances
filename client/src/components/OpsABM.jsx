28b92e916a7a6e89bdc1959d329cedfe3f2f1e5c
Juan Roberts <juancroberts31@gmail.com>
2021-10-01 01:35:32 -0300

    Push from client dir controlled form changes.

--- client/src/components/OpsABM.jsx
+++ client/src/components/OpsABM.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
+import EntryForm from './EntryForm';
 
 export default function ABM() {
     return (
         <div>
             <h1>ABM Operations</h1>
-            <div>
-                <h3>New Entry</h3>
-                <form>
-                    <div>
-                        <h4>Operation:</h4>
-                        <input name="reason" type="text" placeholder="Reason..." />
-                        <input name="amount" type="number" min="0.1" placeholder="Amount..." />
-                    </div>
-                    <div>
-                        <h4>Operation Date:</h4>
-                        <input name="date" type="date" />
-                    </div>
-                    <div>
-                        <h4>Operation Type:</h4>
-                        <label>Addtion</label>
-                        <input name="addition" value="add" type="checkbox" />
-                        <label>Extraction</label>
-                        <input name="extraction" value="rem" type="checkbox" />
-                    </div>
-                    <button>Add</button>
-                </form>
-            </div>
+            <EntryForm />
             <div>
                 <div>
                     <h4>Last Extractions</h4>
