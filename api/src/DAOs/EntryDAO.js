5a4bc9a8b5fab50dd50c2c5ebbd830e59f0294ce
Juan Roberts <juancroberts31@gmail.com>
2021-10-18 20:16:30 -0300

    DB connection /delete entry refactorized. Functioning.

--- api/src/DAOs/EntryDAO.js
+++ api/src/DAOs/EntryDAO.js
@@ -4,53 +4,56 @@ const toolkit = require('../toolkit');
 class EntryDAO {
     constructor () {
         this.addEntryQuery = "INSERT INTO entries(reason, amount, date, type) VALUES($1, $2, $3, $4)";
+        this.selectLastTen = "SELECT * FROM entry ORDER BY date DESC FETCH FIRST 10 ROWS ONLY";
+        this.updateEntry = "UPDATE entry SET $1='$2' WHERE id='$3'";
+        this.deleteEntryById = "DELETE FROM entry WHERE id='$1'"
     }
 
     async createEntry(entry) {
         const {reason, amount, date, type} = entry;
         const values = [reason, amount, date, type]
         const dbEntry = await db.query(this.addEntryQuery, values);
-        console.log(dbEntry);
         return dbEntry;
     }
 
     async getLastEntries() {
         try {
-            const [lastEntries, metadata] = await db.query("SELECT * FROM entry ORDER BY date DESC FETCH FIRST 10 ROWS ONLY");
+            const lastEntries = await db.query("SELECT * FROM entry ORDER BY date DESC FETCH FIRST 10 ROWS ONLY");
  
             if (lastEntries.length > 0) {
                 console.log(JSON.stringify(lastEntries));
             } else {
-                return toolkit.messages.error
+                return toolkit.error
             }
             
             return lastEntries;
         } catch (e) {
             console.error(e);
-            return toolkit.messages.error;
+            return toolkit.error;
         };
     }
 
     async modifyEntry(id, entry) {
         try {
-            for (let i = 0; i < entry.length; i++) {
-                await db.query(`UPDATE entry SET ${entry[i][0]}='${entry[i][1]}' WHERE id='${id}'`);
-            };
+            const values = [id];
+
+            await db.query(this.UpdateEntry);
 
             return "Entry updated successfully.";
         } catch (e) {
             console.error(e);
-            return toolkit.messages.error;
+            return toolkit.error;
         }
     }
 
     async deleteEntry(id) {
         try {
-            await db.query(`DELETE FROM entry WHERE id='${id}'`);
+            const values = [id];
+            await db.query(this.deleteEntryById, values);
             return "Entry deleted succesfully.";
         } catch (e) {
             console.error(e);
-            return toolkit.messages.error;
+            return toolkit.error;
         };
     }
 }
